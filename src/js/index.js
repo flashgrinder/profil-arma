b4496e8bdfe2e7834fac8d955b37f1c96bdb100d
flashgrinder <flashgrinder92@gmail.com>
2025-01-09 12:49:36 +0400

    upd base template

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import mobileNav from './modules/mobile-nav.js';
-mobileNav();
 
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    mobileNav();
+    Fancybox.bind('[data-fancybox]');
+
+})
 // import isAvifWebp from 'avif-webp-checker';
 // isAvifWebp({ mode: 'webp' });
