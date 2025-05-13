274a85c27861fa16bc7dbbcbb4c135a2b736a42e
flashgrinder <flashgrinder92@gmail.com>
2025-05-13 17:29:54 +0400

    fix upload files

--- src/js/modules/form-files.js
+++ src/js/modules/form-files.js
@@ -1,51 +1,39 @@
 function  uploadFiles() {
 
-    const forms = document.querySelectorAll(".js-form");
-    const inputFile = document.querySelectorAll(".js-file");
+    const forms = Array.from(document.querySelectorAll(".js-form"));
+    let btnFile = document.querySelectorAll('.js-file');
+    let textSelectorFile = document.querySelectorAll(".js-name-file");
 
-    /////////// Кнопка «Прикрепить файл» ///////////
-    inputFile.forEach(function(el) {
-        let textSelector = document.querySelector(".js-name-file");
+    forms.forEach((form, index)=> {
         let fileList;
 
-        // Событие выбора файла(ов)
-        el.addEventListener("change", function (e) {
+        btnFile[index].addEventListener('change', (file)=> {
 
-            // создаём массив файлов
             fileList = [];
-            for (let i = 0; i < el.files.length; i++) {
-                fileList.push(el.files[i]);
+            for (let i = 0; i < btnFile[index].files.length; i++) {
+                fileList.push(btnFile[index].files[i]);
             }
 
-            // вызов функции для каждого файла
             fileList.forEach(file => {
-                uploadFile(file);
+                if (file.size > 5 * 1024 * 1024) {
+                    alert("Файл должен быть не более 5 МБ.");
+                    return;
+                }
+
+                // Показ загружаемых файлов
+                if (file && file.length > 1) {
+                    if ( file.length <= 4 ) {
+                        textSelectorFile[index].textContent = `Выбрано ${file.length} файла`;
+                    }
+                    if ( file.length > 4 ) {
+                        textSelectorFile[index].textContent = `Выбрано ${file.length} файлов`;
+                    }
+                } else {
+                    textSelectorFile[index].textContent = file.name;
+                }
             });
-        });
-
-        // Проверяем размер файлов и выводим название
-        const uploadFile = (file) => {
-
-            // файла <5 Мб
-            if (file.size > 5 * 1024 * 1024) {
-                alert("Файл должен быть не более 5 МБ.");
-                return;
-            }
-
-            // Показ загружаемых файлов
-            if (file && file.length > 1) {
-                if ( file.length <= 4 ) {
-                    textSelector.textContent = `Выбрано ${file.length} файла`;
-                }
-                if ( file.length > 4 ) {
-                    textSelector.textContent = `Выбрано ${file.length} файлов`;
-                }
-            } else {
-                textSelector.textContent = file.name;
-            }
-        }
-
-    });
+        })
+    })
 
 }
 
